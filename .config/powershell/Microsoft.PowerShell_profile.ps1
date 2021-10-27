4d67cd0eb7df45e4129ec734c50c93c8f5586c1a
Renegade-Master <ciaran.bent@protonmail.ch>
2021-10-27 19:43:23 +0100

    Added PowerShell Configuration file;

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+### PowerShell User Configuration File ###
+
+## Themes ##
+# If theme is not working on fresh install, ensure to first run
+#  Install-Module oh-my-posh
+
+Import-Module oh-my-posh
+Update-Module oh-my-posh
+Set-PoshPrompt -Theme fish
+
