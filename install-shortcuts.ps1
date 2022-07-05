fe8ce532ee1d584deb1374555bf15290026cf6b0
Renegade-Master <renegade.master.dev@protonmail.com>
2022-07-05 22:52:28 +0100

    Added error checking

--- install-shortcuts.ps1
+++ install-shortcuts.ps1
@@ -1,67 +1,102 @@
 #!/usr/bin/env pwsh
 
 Param (
-    [Parameter(Mandatory=$false)]
-    [Switch]$DryRun
+    [ Parameter(Mandatory = $false) ]
+    [ Switch ]$DryRun
 )
 
 $Config = @{
+    "GitConfig" = @{
+        "Main" = "$( $pwd.Path )/.gitconfig"
+        "Windows" = "$( $env:HOME )\\.gitconfig"
+        "Linux" = "$( $env:HOME )/.gitconfig"
+    }
+    "GitIgnore" = @{
+        "Main" = "$( $pwd.Path )/.gitignore"
+        "Windows" = "$( $env:HOME )/.gitignore"
+        "Linux" = "$( $env:HOME )/.gitignore"
+    }
+    "HTOP" = @{
+        "Main" = "$( $pwd.Path )/.config/htop/htoprc"
+        "Windows" = "NOT_APPLICABLE"
+        "Linux" = "$( $env:HOME )/.config/htop/htoprc"
+    }
+    "NVIM" = @{
+        "Main" = "$( $pwd.Path )/.config/nvim/init.vim"
+        "Windows" = "$( $env:LocalAppData )\\nvim\\init.vim"
+        "Linux" = "$( $env:HOME )/.config/nvim/init.vim"
+    }
     "PowerShell" = @{
-        "Main" = "$($pwd.Path)/.config/powershell/Microsoft.PowerShell_profile.ps1"
+        "Main" = "$( $pwd.Path )/.config/powershell/Microsoft.PowerShell_profile.ps1"
         "Windows" = "$Profile"
         "Linux" = "$Profile"
     }
     "Tmux" = @{
-        "Main" = "$($pwd.Path)/.tmux.conf"
+        "Main" = "$( $pwd.Path )/.tmux.conf"
         "Windows" = "NOT_APPLICABLE"
-        "Linux" = "$($env:HOME)/.tmux.conf"
+        "Linux" = "$( $env:HOME )/.tmux.conf"
     }
     "UserConfig" = @{
-        "Main" = "$($pwd.Path)/.usrrc"
+        "Main" = "$( $pwd.Path )/.usrrc"
         "Windows" = "NOT_APPLICABLE"
-        "Linux" = "$($env:HOME)/.usrrc"
+        "Linux" = "$( $env:HOME )/.usrrc"
     }
     "Vim" = @{
-        "Main" = "$($pwd.Path)/.vimrc"
-        "Windows" = "$($env:HOME)/.vimrc"
-        "Linux" = "$($env:HOME)/.vimrc"
+        "Main" = "$( $pwd.Path )/.vimrc"
+        "Windows" = "$( $env:HOME )\\.vimrc"
+        "Linux" = "$( $env:HOME )/.vimrc"
+    }
+    "WindowsTerminal" = @{
+        "Main" = "$( $pwd.Path )/terminals/windows-terminal/settings.json"
+        "Windows" = "$( $env:LocalAppData )\\Packages\\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\\LocalState\\settings.json"
+        "Linux" = "NOT_APPLICABLE"
     }
 }
 
 Function Link-ConfigurationFile {
     Param (
-        [Parameter(Mandatory=$true)]
-        [String]$LinkFile,
+        [ Parameter(Mandatory = $true) ]
+        [ String ]$LinkFile,
 
-        [Parameter(Mandatory=$true)]
-        [String]$TargetFile,
+        [ Parameter(Mandatory = $true) ]
+        [ String ]$TargetFile,
 
-        [Parameter(Mandatory=$false)]
-        [Boolean]$DryRun
+        [ Parameter(Mandatory = $false) ]
+        [ Boolean ]$DryRun,
+
+        [ Parameter(Mandatory = $false) ]
+        [ Switch ]$Force
     )
+
+    $NoCommit =
+    $ForceOperation = ""
 
     if ($DryRun) {
         Write-Host "Dry Run TRUE"
-        New-Item -Type SymbolicLink -Path $LinkFile -Target $TargetFile -WhatIf
-    } else {
-        Write-Host "Dry Run FALSE"
-        New-Item -Type SymbolicLink -Path $LinkFile -Target $TargetFile -WhatIf
+        $NoCommit = "-WhatIf"
     }
+
+    if ($Force) {
+        $ForceOperation = "-Force"
+    }
+
+    Write-Host "Executing command: [ New-Item -Type SymbolicLink -Path $LinkFile -Target $TargetFile $NoCommit $ForceOperation ]"
+    New-Item -Type SymbolicLink -Path $LinkFile -Target $TargetFile $ForceOperation $NoCommit
 }
 
 
 Function Link-ConfigurationFiles {
     Param (
-        [Parameter(Mandatory=$false)]
-        [Boolean]$DryRun
+        [ Parameter(Mandatory = $false) ]
+        [ Boolean ]$DryRun
     )
     $HostOs = "DEFAULT"
 
-    if ($PSVersionTable.OS.Contains("Windows")) {
+    if ( $PSVersionTable.OS.Contains("Windows")) {
         $HostOs = "Windows"
     }
 
-    if ($PSVersionTable.OS.Contains("Linux")) {
+    if ( $PSVersionTable.OS.Contains("Linux")) {
         $HostOs = "Linux"
     }
 
@@ -77,9 +112,22 @@ Function Link-ConfigurationFiles {
             continue
         }
 
-        Write-Host "`nLinking $($Replacement): [$($Config.$Replacement.$HostOs)] to [$($Config.$Replacement.Main)]."
+        Write-Host "`nLinking $( $Replacement ): [$( $Config.$Replacement.$HostOs )] to [$( $Config.$Replacement.Main )]."
 
-        Link-ConfigurationFile -LinkFile $($Config.$Replacement.$HostOs) -TargetFile $($Config.$Replacement.Main) -DryRun $DryRun
+        try {
+            Link-ConfigurationFile -LinkFile $( $Config.$Replacement.$HostOs ) -TargetFile $( $Config.$Replacement.Main ) -DryRun $DryRun
+        }
+        catch [ Exception ] {
+            $emessage = $_
+            Write-Host "Error: $($Error[0].Exception.GetType().FullName)"
+            Write-Host "E: $emessage"
+
+            $continue = Read-Host "That didn't appear to work. Would you like to attempt to force the operation? Y/N"
+
+            if ($continue -eq "Y" -or $continue -eq "y") {
+                Link-ConfigurationFile -LinkFile $( $Config.$Replacement.$HostOs ) -TargetFile $( $Config.$Replacement.Main ) -DryRun $DryRun -Force
+            }
+        }
     }
 }
 
