fb659ca73fd655c599353bdf27df96b537ffd65a
Renegade-Master <renegade@renegade-master.com>
2026-09-02 12:40:07 +0100

    Modify Packer.nvim install command for efficiency
    Updated the Packer.nvim installation command to use --depth 1 for faster cloning.
    
    Signed-off-by: Renegade-Master <renegade@renegade-master.com>

--- editors/nvim/lua/plugins.lua
+++ editors/nvim/lua/plugins.lua
@@ -1,5 +1,6 @@
 -- Packer Install Command:
---   git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+--   git clone --depth 1 https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+--   git clone --depth 1 https://github.com/wbthomason/packer.nvim "~/.local/share/nvim/site/pack/packer/start/packer.nvim"
 
 -----------------
 -- Packer.nvim --
